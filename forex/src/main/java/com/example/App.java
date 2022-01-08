f6ef2e778f12dad17c32524feced3b26991c3312
Metalam <metalam85@gmail.com>
2022-01-08 14:04:44 +0200

    chromedriver

--- forex/src/main/java/com/example/App.java
+++ forex/src/main/java/com/example/App.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Hello world!
@@ -8,23 +13,43 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
  */
 public class App 
 {
-    public static void main( String[] args )
+    
+
+    public static void main( String[] args ) throws InterruptedException 
     {
-        Car newCar1 = new Car();
-        Car newCar2 = new Car();
+        String articles = "//table/tbody/tr/td[1]";
+        System.setProperty("webdriver.chrome.driver", "C:/repos/forexGrow/forex/src/resources/chromedriver.exe"); 
+        WebDriver driver = new ChromeDriver();
 
-        newCar2.color = "red";
+        driver.get("https://rf-start.com");
 
-        System.out.println( newCar1.color);
-        System.out.println( newCar1.fuel);
-        newCar1.availebleDistance();
+        driver.manage().window().maximize();
 
-        System.out.println("");
+        driver.findElements(By.xpath(articles))
+
+        Thread.sleep(5000);
+        driver.quit();
+        driver.close();
+        // Car newCar1 = new Car();
+        // Car newCar2 = new Car();
+
+        // newCar2.color = "red";
+
+        // System.out.println( newCar1.color);
+        // System.out.println( newCar1.fuel);
+        // newCar1.availebleDistance();
+
+        // System.out.println("");
 
 
-        System.out.println( newCar2.color);
-        System.out.println( newCar2.fuel);
-        newCar2.availebleDistance();
+        // System.out.println( newCar2.color);
+        // System.out.println( newCar2.fuel);
+        // newCar2.availebleDistance();
 
+    }
+
+    //@Test
+    public void firstTest() throws InterruptedException{
+        
     }
 }
