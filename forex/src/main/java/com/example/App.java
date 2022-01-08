5559ca1cbeabd537c1a8bef0a89424569811ede9
Metalam <metalam85@gmail.com>
2022-01-08 16:56:44 +0200

    chrome for linux

--- forex/src/main/java/com/example/App.java
+++ forex/src/main/java/com/example/App.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import java.util.List;
+
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import net.jodah.failsafe.internal.util.Assert;
 
 /**
  * Hello world!
@@ -18,18 +22,37 @@ public class App
     public static void main( String[] args ) throws InterruptedException 
     {
         String articles = "//table/tbody/tr/td[1]";
-        System.setProperty("webdriver.chrome.driver", "C:/repos/forexGrow/forex/src/resources/chromedriver.exe"); 
+        String OS_Linux = "Linux";
+        String OS_Windows = "Windows";
+        String OS;
+
+        OS = OS_Windows;
+        if(OS==OS_Windows){
+            System.setProperty("webdriver.chrome.driver", "./forex/src/resources/chromedriver.exe");
+        }
+
+        if(OS==OS_Linux){
+            System.setProperty("webdriver.chrome.driver", "./forex/src/resources/chromedriver");
+        }
+         
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://rf-start.com");
 
         driver.manage().window().maximize();
 
-        driver.findElements(By.xpath(articles))
+        List<WebElement> listOfElements = driver.findElements(By.xpath(articles));
 
-        Thread.sleep(5000);
+        for (WebElement webElement : listOfElements) {
+            String text = webElement.getText();
+            System.out.println(text);
+            Assert.isTrue(!webElement.getText().contains("Taras"), "String doesn't contain specific text");
+        }
+
+        Thread.sleep(10000);
         driver.quit();
         driver.close();
+
         // Car newCar1 = new Car();
         // Car newCar2 = new Car();
 
@@ -48,8 +71,8 @@ public class App
 
     }
 
-    //@Test
+    @Test
     public void firstTest() throws InterruptedException{
-        
+       
     }
 }
